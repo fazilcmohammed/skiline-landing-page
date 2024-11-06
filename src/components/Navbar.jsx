d1282ea79cd95b9484b9cf168e02b1a0818ac9aa
fazilcmohammed <mohammedfazilc786@gmail.com>
2024-11-06 13:51:09 +0530

    created navbar and added logo, nav links and two buttons for loging and joining

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FaArrowRight } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <nav className='bg-[#FFF2F3] px-28 py-5'>
+        <div className='flex items-center justify-between'>
+            <h1 className='text-2xl font-bold text-[#252B42]'>skiline</h1>
+            <ul className='text-[#737373] text-[16px] font-semibold flex'>
+                <li className='mr-10'><a href="#">Home</a></li>
+                <li className='mr-10'><a href="#">Product</a></li>
+                <li className='mr-10'><a href="#">Pricing</a></li>
+                <li className='mr-10'><a href="#">Contact</a></li>
+            </ul>
+            <div className='flex items-center gap-3'>
+                <button>Login</button>
+                <button className='flex items-center gap-3'>Join us <FaArrowRight/></button>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
